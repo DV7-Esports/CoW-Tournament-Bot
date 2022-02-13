4a9756b74905659ee68363fd2c3a30088690b36c
Alex Tsvetanov <alex_tsvetanov_2002@abv.bg>
2022-02-13 23:36:44 +0200

    add express app and start riot api

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,49 @@
-// Getting and validating .env file
-import EnvLoader from './classes/EnvLoader';
-EnvLoader.load();
+(async () => {
+    // Getting and validating .env file
+    const EnvLoader = require('./classes/EnvLoader').default;
+    EnvLoader.load();
+    // Setting up moment-timezone
+    const moment = require('moment-timezone');
+    // Starting client
+    const client = require('./client').default;
+    moment.locale('en');
+    moment.tz.setDefault('Europe/Sofia');
+    client.login(client.config.token);
+})();
 
-// Setting up moment-timezone
-import moment from 'moment-timezone';
-moment.locale('en');
-moment.tz.setDefault('Europe/Sofia');
+(async () => {
+    // Getting Express app
+    const app = require("./express/src/app").default;
+    
+    const port = app.get("port");
 
-// Starting client
-import client from './client';
-
-client.login(client.config.token);
+    const server = app.listen(port, onListening);
+    server.on("error", onError);
+    
+    function onError(error: NodeJS.ErrnoException) {
+        if (error.syscall !== "listen") {
+            throw error;
+        }
+    
+        const bind = typeof port === "string" ? `Pipe ${port}` : `Port ${port}`;
+    
+        // handle specific listen errors with friendly messages
+        switch (error.code) {
+            case "EACCES":
+                console.error(`${bind} requires elevated privileges`);
+                break;
+            case "EADDRINUSE":
+                console.error(`${bind} is already in use`);
+                break;
+            default:
+                throw error;
+        }
+    }
+    
+    function onListening() {
+        const addr = server.address();
+        const bind =
+                typeof addr === "string" ? `pipe ${addr}` : `port ${addr?.port}`;
+        console.log(`Listening on ${bind}`);
+    }  
+})();
