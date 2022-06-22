464c77025e7efcbd6abaa4d9ba8fbdcfe544575b
Alex Tsvetanov <14363901+Alex-Tsvetanov@users.noreply.github.com>
2022-06-22 16:11:35 +0300

    match recording

--- src/express/src/routes/tournament.ts
+++ src/express/src/routes/tournament.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import RiotApi from "../../../utils/riot/api/main";
 import db from '../../../structures/db';
+import Match from "../../../structures/Match";
 
 export const tournament = Router();
 
@@ -9,11 +10,28 @@ tournament.post('/match/record', (request, response) => {
     const matchId = params['region'] + '_' + params['gameId'];
     try {
         const matchData: any = JSON.parse(RiotApi.getInstance().getMatchData(matchId));
-        db((tables) => {
-            tables.matches.insertOne(matchData);
+        db(async (tables) => {
+            await tables.match_stats.insertOne(matchData);
+            await tables.matches.findOneAndUpdate(   
+                { "tournamentCode" : params['code'] },
+                { $set: { "finished" : true } }
+            );
         });
     } catch (e: any) {
         response.sendStatus(404);
     }
     response.send('Tournament code record saved.');
+});
+
+tournament.get('/schedule', (request, response) => {
+    const params: any = request.params;
+    try {
+        let matches: Match[] = [];
+        db(async (tables) => {
+            matches = await tables.matches.find( { 'finished': false } ).toArray();
+        });
+        response.json(matches);
+    } catch (e: any) {
+        response.sendStatus(404);
+    }
 });
--- src/utils/teamUtils.ts
+++ src/utils/teamUtils.ts
@@ -1,7 +1,5 @@
-import { Message, TextChannel } from "discord.js";
-import client from "../client";
+import { Collection, GuildMember, Message, TextChannel } from "discord.js";
 import db from "../structures/db";
-import DiscordClient from "../structures/DiscordClient";
 import Ign from "../structures/Ign";
 import Player from "../structures/Player";
 import { Region } from "../structures/Region";
@@ -9,7 +7,25 @@ import Team from "../structures/Team";
 import TeamRequest from "../structures/TeamRequest";
 import constants from "./constants";
 
-async function rosterGenerator(team: Team, request?: boolean, validator?: boolean) {
+function getDiscordIdByDiscordName(fetchedMemebers: Collection<string, GuildMember> | undefined, discordName: string): string {
+    const memberId: string | undefined = fetchedMemebers?.find(member => member.user.username === discordName.split('#')[0] &&
+                                            member.user.discriminator === discordName.split('#')[1])?.id;
+    if (!memberId) {
+        return discordName;
+    } else {
+        return memberId as string;
+    }
+}
+
+function mentionOrList(discordUser: string): string {
+    if (discordUser.indexOf('#') === -1) {
+        return discordUser;
+    } else {
+        return `<@${discordUser}>`;
+    }
+}
+
+async function rosterGenerator(team: Team, request?: boolean, validator?: boolean): Promise<string> {
     let roster = '';
 
     if (validator === true || validator === undefined) roster = `<@&${constants.accountValidatorRoleId}>\n`;
@@ -19,8 +35,8 @@ async function rosterGenerator(team: Team, request?: boolean, validator?: boolea
                                               else teamToPrint = team;
     teamToPrint.requestor = team.request.requestor;
 
-    teamToPrint.manager = teamToPrint.managers.map((x: string) => `<@${x}>`).join(' ');
-    teamToPrint.captain = teamToPrint.captains.map((x: string) => `<@${x}>`).join(' ');
+    teamToPrint.manager = teamToPrint.managers.map((x: string) => `${mentionOrList(x)}`).join(' ');
+    teamToPrint.captain = teamToPrint.captains.map((x: string) => `${mentionOrList(x)}`).join(' ');
 
     roster = roster + `Team ${teamToPrint.name} (${teamToPrint.abbr})`;
 
@@ -30,7 +46,7 @@ async function rosterGenerator(team: Team, request?: boolean, validator?: boolea
                     + `\n\`\`Team captains:\`\` ${teamToPrint.captain}\n`;
 
     teamToPrint.players.forEach((player: Player) => {
-        roster = roster + `\`\`       Player:\`\` <@${player.user}>\n`;
+        roster = roster + `\`\`       Player:\`\` ${mentionOrList(player.user)}\n`;
         player.igns.forEach((x: Ign) => {
             roster = roster + `\`\`              \`\` <https://www.leagueofgraphs.com/summoner/${x.region.toLowerCase()}/${encodeURIComponent(x.summoner)}>\n`;
         });
@@ -41,16 +57,26 @@ async function rosterGenerator(team: Team, request?: boolean, validator?: boolea
     return roster;
 }
 
+function getRegionByName (regionName: string): Region {
+    const regions: string[] = Object.keys(Region).filter((item) => {
+        return isNaN(Number(item));
+    });
+    if (regions.includes(regionName)) {
+        return regionName as Region;
+    }
+    else {
+        return Region.INVALID;
+    }
+}
+
 async function generateTeam(reply: Message, args : string[]): Promise<Team> {
     let fetchedMemebers = await reply.guild?.members.fetch();
     let name: string, abbrev: string, color: string, managers: string[], captains: string[], members: Player[];
     name = args[0];
     abbrev = args[1];
     color = args[2];
-    managers = [fetchedMemebers?.find(member => member.user.username === args[3].split('#')[0] &&
-                                        member.user.discriminator === args[3].split('#')[1])?.id as string];
-    captains = [fetchedMemebers?.find(member => member.user.username === args[4].split('#')[0] &&
-                                        member.user.discriminator === args[4].split('#')[1])?.id as string];
+    managers = [getDiscordIdByDiscordName(fetchedMemebers, args[3])];
+    captains = [getDiscordIdByDiscordName(fetchedMemebers, args[4])];
     
     let warningMessage = '';
     members = args.slice(5).reduce((array, value, index) => {
@@ -65,19 +91,17 @@ async function generateTeam(reply: Message, args : string[]): Promise<Team> {
         }
         return array;
     }, [] as {'user': string, 'igns': string}[]).map(users => {
-        const userId: string = fetchedMemebers?.find(member => member.user.username === users['user'].split('#')[0] &&
-                                                        member.user.discriminator === users['user'].split('#')[1])?.id as string;
+        const userId: string = getDiscordIdByDiscordName(fetchedMemebers, users['user']);
         const igns: Ign[] = users['igns']
             .split(',')
             .map(x => x.trim())
             .map(ign => {
                 let accountInfo: Ign = {
                     summoner: ign.split('#')[0],
-                    region: ign.split('#')[1].toUpperCase()
+                    region: getRegionByName(ign.split('#')[1].toUpperCase())
                 } as Ign;
 
-                if (!accountInfo.region) {
-                    accountInfo.region = Region.INVALID;
+                if (accountInfo.region === Region.INVALID) {
                     warningMessage += `${accountInfo.summoner} **(${accountInfo.region})** is not from a valid region.\n`;
                 }
 
